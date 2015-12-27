ebf988b0e5474ba403f173ef532e5f43678c053e
Lars Wirzenius <liw@liw.fi>
2015-12-27 21:06:59 +0100

    Prepare release 0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup, Extension
 import glob
 
 setup(name='obbench',
-      version='0.2',
+      version='0.3',
       description='Obnam benchmarking',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
